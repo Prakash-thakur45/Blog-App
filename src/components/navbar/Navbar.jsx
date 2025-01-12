a7e2f3659741592672f7806c120c24a3eecbc6ad
Prakash Thakur <160341516+Prakash-thakur45@users.noreply.github.com>
2025-01-12 16:19:33 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Links from "./links/Links";
-import styles from './Navbar.module.css'
+import styles from './navbar.module.css'
 import { auth } from "@/lib/auth";
 const Navbar=async()=>{
   const session=await auth();
